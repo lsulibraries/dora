1f4049f2b827841757b9bac9f53550bdd41c2019
Jason Peak <jpeak5@lsu.edu>
2017-12-05 17:11:16 -0600

    adds some behat stuff.

---
+++ behat.sh
@@ -0,0 +1,32 @@
+
+#!/bin/sh
+#set -e
+
+if [ -e /.installed ]; then
+  echo 'Already installed.'
+
+else
+  echo ''
+  echo 'INSTALLING'
+  echo '----------'
+
+  # Install Java, Firefox, Xvfb, and unzip
+  apt-get -y install openjdk-7-jre-headless firefox xvfb unzip
+
+  wget "http://selenium-release.storage.googleapis.com/2.42/selenium-server-standalone-2.42.1.jar"
+  mv selenium-server-standalone-2.42.1.jar /usr/local/bin
+
+  # So that running `vagrant provision` doesn't redownload everything
+  touch /.installed
+fi
+
+# Release old locks
+echo "Releasing old XVFB locks from /tmp..."
+cd /tmp
+sudo rm -rf .X*-lock
+
+echo "Starting Selenium Server. Default screen resolution: 1280x1024."
+cd /usr/local/bin
+DISPLAY=:1 xvfb-run --server-args="-screen 0 1280x1024x8" java -jar ./selenium-server-standalone-2.42.1.jar &
+
+echo "Give Selenium Server a few minutes to fireup, then run your BDD tests."
